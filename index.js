27a4900c7fe0410f369aca33a91cb139022461d4
Eric Jackson <ejackson@ashevillenc.gov>
2016-11-16 14:49:49 -0500

    Final?

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Tabletop = require('tabletop');
 const Utilities = require('./utilities.js');
 const fs = require('fs');
+var parse = require('csv-parse/lib/sync');
+
+let permitNum = -1;
 
 const createTask = function(process, task, status, start, end, due, owner, level) {
   let days = -1;
@@ -9,7 +12,7 @@ const createTask = function(process, task, status, start, end, due, owner, level
     const d2 = new Date(due);
     days = Utilities.workingDaysBetweenDates(d1, d2);
   }
-  return { process, task, status, start, end, due, days, owner, level };
+  return { B1_ALT_ID: permitNum, process, task, status, start, end, due, days, owner, level };
 }
 
 const resetAllTasks = function (currentProcessState, statusDate) {
@@ -28,91 +31,89 @@ const applicationProcess = function (tasks, process, row, currentProcessState) {
 
   if (!('processStartDate' in currentProcessState)) { // First time here
     currentProcessState.processStartDate = statusDate; // This is the start of the whole process
-    currentProcessState.processRoundStartDate = null; // Start of the current round (probably not relevant here)
+    currentProcessState.processRoundStartDate = null; // Start of the current round
     tasks.push(createTask(process, process, 'Start', statusDate, statusDate, null, '-', 0));
   }
 
-  if (task == process) {
-    if (status == 'Contacted Applicant') {
-      /*
-      * I'm not sure this is right. This means that any new requirements
-      * will be considered to have been imposed at this point.
-      * I thought about also resetting all the tasks, but that ends up
-      * creating duplicate 'Required' lines that don't make sense.
-      *
-      * Worth asking Diane. It may be better to just consider that all requirements
-      * date from the point where we set Conditions of Approval.
-      */
-      currentProcessState.processRoundStartDate = statusDate;
-    }
+  /*
+   * Special tasks: these tasks start, reset or end the process
+   */
+  const specials = { 'Application Process': 0, 'Conditions of Approval': 1 };
 
+  if (task in specials) {
+    /*
+     * Not 100% sure this is right. A new "round" starts not only at the start
+     * of the process and when 'Conditions of Approval' status occurs, but also
+     * whenever a 'Contacted Applicant' status occurs.
+     *
+     * Worth asking Diane. It may be better to just consider that all requirements
+     * date from the point where we set Conditions of Approval.
+     */
+    currentProcessState.processRoundStartDate = statusDate;
+    if (task == 'Conditions of Approval') {
+      resetAllTasks(currentProcessState, statusDate);
+    }
     if (status == 'Complete') {
       currentProcessState.complete = true;
-      tasks.push(createTask(process, task, status, currentProcessState.processStartDate, statusDate, null, '-', 0));
+      tasks.push(createTask(process, task, status, currentProcessState.processStartDate, statusDate, null, '-', specials[task]));
     }
     else {
-      tasks.push(createTask(process, task, status, statusDate, statusDate, null, '-', 0));
+      tasks.push(createTask(process, task, status, statusDate, statusDate, null, '-', specials[task]));
     }
+    return;
   }
-  else if (task == 'Conditions of Approval') {
-    currentProcessState.processRoundStartDate = statusDate; // We use this to initialize tasks that aren't yet created
-    resetAllTasks(currentProcessState, statusDate);
-  }
-  else if (! (task in currentProcessState)) {
+
+  /*
+   * If we're here, we have a regular task
+   */
+  if (! (task in currentProcessState)) { // Initialize this task if we see it for the furst time
     currentProcessState[task] = {
       reset: true,
       start: currentProcessState.processRoundStartDate?currentProcessState.processRoundStartDate:statusDate,
-      previous: null};
+      previous: null
+    };
   }
 
+  const terminators = { 'Provided': 1, 'Accepted - within 10% of ICC Value':1, 'Approved':1,
+                        'Approved - Pending Payment':1,
+                        'Disapproved':1, 'Final Release':1, 'Incomplete - Partial Approval':1,
+                        'Backflow Device Not Required':1, 'Backflow Device Required':1 };
+  const waits = { 'Sent': 'THIRD', 'NULL':'CUST', 'Approval Required':'CUST',
+                  'BC Septic Approval Required':'CUST',
+                  'BC Septic Approval Req.':'CUST',
+                  'Hold - See Comment':'CUST', 'Hold - See Comments':'CUST',
+                  'Required':'CUST',
+                  'Verification Requested':1 };
   let switchStatus = status;
-  if (status.startsWith('Accepted')) switchStatus = 'Accepted';
-  let owner = 'CUST';
-  let level = 1;
+  if (status in terminators) switchStatus = 'TERMINATOR';
+  if (status in waits) switchStatus = 'WAITS';
+
+  // Complete the previous step, if it exists
+  if (currentProcessState[task].previous) {
+    currentProcessState[task].previous.end = statusDate;
+    currentProcessState[task].previous = null;
+  }
+
   switch (switchStatus) {
-    case 'Required':
+    case 'WAITS':
       {
         if (currentProcessState[task].reset) {
           currentProcessState[task].reset = false;
-          tasks.push(createTask(process, task, 'Required', statusDate, null, null, owner, level));
+          tasks.push(createTask(process, task, status, statusDate, null, null, waits[status], 1));
           currentProcessState[task].start = statusDate;
           currentProcessState[task].previous = tasks[tasks.length-1];
         }
       }
-    break;
-    case 'Sent':
-     {
-       if (currentProcessState[task].previous) {
-         currentProcessState[task].previous.end = statusDate;
-       }
-       currentProcessState[task].reset = false;
-       tasks.push(createTask(process, task, 'Sent', statusDate, statusDate, null, 'THIRD', level));
-       currentProcessState[task].start = statusDate;
-       currentProcessState[task].previous = tasks[tasks.length-1];
-     }
-     break;
+      break;
 
-   case 'Provided':
-   case 'Accepted':
-   case 'Approved':
+    case 'TERMINATOR':
     {
-      if (currentProcessState[task].reset) {
-        let insertedStatus = 'Required';
-        let insertedOwner = owner;
-        if (task == 'Air Quality Approval') {
-          insertedStatus = 'Sent';
-          insertedOwner='THIRD';
-        }
-        tasks.push(createTask(process, task, insertedStatus, currentProcessState[task].start, statusDate, null, insertedOwner, level));
-      }
-      if (currentProcessState[task].previous) {
-        currentProcessState[task].previous.end = statusDate;
-      }
-      tasks.push(createTask(process, task, status, statusDate, statusDate, null, owner, level));
+      tasks.push(createTask(process, task, status, statusDate, statusDate, null, 'CUST', 1));
     }
     break;
 
     default:
+      console.log(permitNum + ": Unknown Application Process task status: " + process + "." + task + "." + status);
       break;
   }
 }
@@ -213,6 +214,16 @@ const reviewProcess = function (tasks, process, row, currentProcessState) {
        currentProcessState[task].previous = tasks[tasks.length-1];
      }
      break;
+
+    case 'NULL':
+      {
+        tasks.push(createTask(process, task, 'Pending Review', currentProcessState.processRoundStartDate, null, null, 'DSD', level));
+        currentProcessState[task].reset = false;
+        currentProcessState[task].start = currentProcessState.processRoundStartDate;
+        currentProcessState[task].previous = tasks[tasks.length-1];
+      }
+      break;
+
     case 'TERMINATOR':
      {
        if (currentProcessState[task].reset) {
@@ -229,7 +240,7 @@ const reviewProcess = function (tasks, process, row, currentProcessState) {
      break;
 
     default:
-      console.log("UNKNOWN REVIEW: " + process + ", task = " + task + ", status = " + status);
+      console.log(permitNum + ": Unknown Review: " + process + ", task = " + task + ", status = " + status + ", date " + statusDate);
     //tasks.push(createTask(process, task, status, statusDate, statusDate, due, owner, level));
 
       break;
@@ -260,7 +271,13 @@ const closeoutProcess = function (tasks, process, row, currentProcessState) {
     tasks.push(createTask(process, task, status, currentProcessState.processStartDate, statusDate, null, '-', 0));
   }
   else if (task != process) {
-    tasks.push(createTask(process, task, status, statusDate, statusDate, null, '-', 1));
+    const locStatus = (status == 'NULL')?'Pending':status;
+    let startDate = statusDate, endDate = statusDate;
+    if (startDate == 'NULL') {
+      startDate = currentProcessState.processStartDate;
+      endDate = null;
+    }
+    tasks.push(createTask(process, task, locStatus, startDate, endDate, null, '-', 1));
   }
 }
 
@@ -312,9 +329,9 @@ const wf_masterv4 = function (elements, output) {
 }
 
 let target_sheet = '16-07528'; // Rocky main
-// target_sheet = '16-07556PZ'; // Rocky planning
-// target_sheet = '16-09014';
-// target_sheet = '16-10083';
+//target_sheet = '16-07556PZ'; // Rocky planning
+//target_sheet = '16-09014';
+//target_sheet = '16-10083';
 let sheet = null;
 
 const processGoogleSpreadsheetData = function(data, tabletop) {
@@ -349,7 +366,7 @@ const processGoogleSpreadsheetData = function(data, tabletop) {
  * MAIN PROGRAM
  */
 
-const processingMode = 'sheets';
+const processingMode = 'csv';
 if (processingMode == 'sheets') {
   const permits_sheet = '1TR3v7jKfw1as8RuXrzvDqwoQdrOltMreqlqwJnxwWDk';
   Tabletop.init( { key: permits_sheet,
@@ -360,6 +377,48 @@ else { // read a csv file
   let input = fs.openSync('masterv4.csv', 'r');
   let data = fs.readFileSync(input);
   let records = parse(data, {columns: true});
+
+  let output = process.stdout.fd;
+  if (process.argv.length > 2) {
+    const fname = (process.argv[2][0] == '/')?process.argv[2]:`./${process.argv[2]}`;
+    output = fs.openSync(fname, 'w');
+  }
+
+  fs.write(output,
+    'B1_ALT_ID,Id,Process,Task,Status,Start,End,Due Date,Owner,Level\n');
+
+  // Now process permit by permit
+  let done = false, cur = 0, id = null, count = 0;
+  let elements = [];
+  while (!done) {
+    if (id == null) id = records[cur].B1_ALT_ID;
+    if (cur >= records.length) done = true;
+    if (done || records[cur].B1_ALT_ID != id) {// Process a permit
+      ++count;
+      let tasks = [];
+      permitNum = elements[0].B1_ALT_ID;
+      if (elements[0].Workflow == 'MASTER V4') {
+        tasks = wf_masterv4(elements, output);
+      }
+      else {
+        console.log("Unknown workflow " + elements[0].Workflow);
+      }
+
+      tasks.forEach( (row, index) => {
+        let line = `${row.B1_ALT_ID},${index},${row.process},${row.task},${row.status},`;
+        line += `${row.start},${row.end},${row.due},${row.owner},${row.level}\n`;
+        fs.writeSync(output, line);
+        //console.log(line);
+      });
+      elements = [];
+      id = null;
+    }
+    else {
+      elements.push(records[cur]);
+      ++cur;
+    }
+    if (cur >= records.length-1) done = true;
+  }
   console.log("The length of the file is " + records.length);
-  console.log("REC: " + JSON.stringify(records[1000]));
+  console.log("Done processing - total permits = " + count);
 }
